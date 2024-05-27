0618c687c5437c820b8e60e24a35e854ba1a39ec
1ucasruan <lucasruan1.tuc@gmail.com>
2024-05-27 13:32:23 -0300

    feat: add encadeamento de .then com arrow function no main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,24 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function (response) {
-    console.log(response)
-})
+//Fazendo requisição através da URL, método tradicional com (function)
+// fetch(url)
+//     .then(function (response) {
+//         return response.json()
+//     })
+//     .then(function (jsonBody) {
+//         console.log(jsonBody)
+//     })
+//     .catch(function (error) {
+//         console.error(error)
+//     })
+//     .finally(function () {
+//         console.log('Requisição concluida!')
+//     })
+
+
+//Fazendo requisição através da URL, método reduzido (=>) (function)
+fetch(url)
+    .then((response) => response.json())    
+    .then((jsonBody) => console.log(jsonBody))   
+    .catch((error) => console.error(error)) 
