d14eb45d58f9fdc79c99446c27d50fc73cee554c
1ucasruan <lucasruan1.tuc@gmail.com>
2024-05-27 15:00:14 -0300

    feat: adicionando pokemons dinamicamente atraves do script main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,13 +17,34 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 //         console.log('Requisição concluida!')
 //     })
 
+function convertPokemonToHtml(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"  alt="${pokemon.name}"/>
+            </div>
+        </li>`
+}
+
+const pokemonList = document.getElementById('pokemonList')
 
 //Fazendo requisição através da URL, método reduzido (=>) (function)
 fetch(url)
-    .then((response) => response.json())    
-    .then((jsonBody) => jsonBody.results)  
-    .then((pokemonList) => {
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
         //debugger
-        console.log(pokemonList)
-    }) 
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+
+            pokemonList.innerHTML += convertPokemonToHtml(pokemon)
+        }
+    })
     .catch((error) => console.error(error)) 
